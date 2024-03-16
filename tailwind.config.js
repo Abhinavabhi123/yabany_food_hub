5296c4de748b2aa0854fe8372a525fd440533e9b
Abhinav <abhinavkinarullathil@gmail.com>
2024-03-16 15:58:48 +0530

    added custome color pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary-color":"#942f42"
+      }
+    },
   },
   plugins: [],
 }
